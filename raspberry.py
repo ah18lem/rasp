6b72546237499017d569fce754d516c88893ee65
Ahlem <56822741+ah18lem@users.noreply.github.com>
2024-05-07 10:29:00 +0200

    Update raspberry.py

--- raspberry.py
+++ raspberry.py
@@ -9,11 +9,10 @@ import constantes
 from sklearn.metrics import classification_report
 # Define the Flower client class
 class AutoencoderClient(fl.client.NumPyClient):
-    def __init__(self, cid, net, train, labels, test):
+    def __init__(self, cid, net, train, labels):
         self.cid = cid
         self.net = net
         self.train = train
-        self.test = test
         self.labels = labels
 
     def get_parameters(self, config):
@@ -26,41 +25,20 @@ class AutoencoderClient(fl.client.NumPyClient):
 
 
     def evaluate(self, parameters, config):
-        self.net.set_parameters(parameters)
-        test_features = self.test
-        test_features = np.array(test_features)
-        _, reconstructed_data = self.net.call(test_features)
-
-        mse = float(
-            np.mean(np.square(reconstructed_data - test_features))
-        )  # Convert to float
-
-        num_samples = len(self.test)  # Replace with the actual number of test samples
-
         # Return the evaluation metrics as a tuple
-        return mse, num_samples, {"mse": mse}
+        return 1, 1, {"mse": 1}
 
-
-def get_client_fn(inputdim,dataset, fake_labels,test):
-
-    def create_client(client_id):
-        client_id = int(client_id)
-       
-        tf_client = AutoencoderClient(client_id,net,dataset[client_id], fake_labels[client_id], test).to_client()
-        return tf_client
-
-    return create_client
 
 net = model.AutoencoderWithClassifier(18,isServer=False, vae=constantes.VAE)
 # Start Flower client setting its associated data partition
 
 data=pd.read_csv("data.csv")  
 fake_labels=pd.read_csv("labels.csv")  
-test=pd.read_csv("test.csv")  
+
 
 fl.client.start_client(
         server_address='192.168.53.247:8080',
         client=AutoencoderClient(
-          0,net,data, fake_labels, test
+          0,net,data, fake_labels
         ).to_client(),
     )
